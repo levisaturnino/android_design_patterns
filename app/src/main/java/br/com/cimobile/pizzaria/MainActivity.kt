21fabe7b4ce0b8796e3736cbb0abfd55274c0772
Levi Saturnino <levisaturnino@gmail.com>
2020-11-12 11:38:45 -0300

    tag

--- app/src/main/java/br/com/cimobile/pizzaria/MainActivity.kt
+++ app/src/main/java/br/com/cimobile/pizzaria/MainActivity.kt
@@ -1,36 +1,28 @@
 package br.com.cimobile.pizzaria
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
     private var pizza: Pizza? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_pizza_calabresa.setOnClickListener {
-            criarPizza("calabresa")
-            tv_pizza.text = pizza?.name()
-        }
-
-        btn_pizza_portuguesa.setOnClickListener {
-            criarPizza("portuguesa")
-            tv_pizza.text = pizza?.name()
-        }
-
-        btn_pizza_quatro_queijo.setOnClickListener {
-            criarPizza("queijo")
-            tv_pizza.text = pizza?.name()
-        }
-
-        btn_pizza_camarao.setOnClickListener {
-            criarPizza("camarao")
-            tv_pizza.text = pizza?.name()
-        }
+        init()
     }
+
+    private fun init() {
+        btn_pizza_calabresa.setOnClickListener(this)
+        btn_pizza_portuguesa.setOnClickListener(this)
+        btn_pizza_quatro_queijo.setOnClickListener(this)
+        btn_pizza_camarao.setOnClickListener(this)
+    }
+
 
     fun criarPizza(tipo: String) {
         if (tipo == "queijo") {
@@ -42,5 +34,21 @@ class MainActivity : AppCompatActivity() {
         } else if (tipo == "camarao") {
             pizza = PizzaCamarao()
         }
+    }
+
+    override fun onClick(view: View?) {
+
+        when (view?.id) {
+            R.id.btn_pizza_calabresa ->
+                criarPizza("calabresa")
+            R.id.btn_pizza_portuguesa ->
+                criarPizza("portuguesa")
+            R.id.btn_pizza_quatro_queijo ->
+                criarPizza("queijo")
+            R.id.btn_pizza_camarao ->
+                criarPizza("camarao")
+        }
+
+        tv_pizza.text = pizza?.name()
     }
 }
